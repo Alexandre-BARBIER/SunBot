5b896b5cfb52f06f596e714753ced64a691bda07
Alexandre BARBIER <alexandre.barbier@student-cs.fr>
2024-02-21 23:32:44 +0100

    add logging + correct mistake

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
     """Remove job with given name. Returns whether job was removed."""
     current_jobs = context.job_queue.get_jobs_by_name(name)
     if not current_jobs:
+        print("Removal failed")
         return False
     for job in current_jobs:
         job.schedule_removal()
@@ -32,11 +33,15 @@ async def moonsticker(context: CallbackContext) -> None:
     update_job_sunset(job.chat_id,context)
 
 def update_job_sunrise(chat_id, context:CallbackContext):
+    
+    print("Update Sunrise | Chat ", chat_id)
+
     # Get Chat from database
     localDatabase = Database()
     chat = localDatabase.get_chat(chat_id)
     
     # Remove previous sunrise job
+    print("Update Sunrise | Try remove sunrise for chat ", chat_id)
     job_removed = remove_job_if_exists(str(chat_id)+"sunrise", context)
 
     # Get current time
@@ -47,22 +52,27 @@ def update_job_sunrise(chat_id, context:CallbackContext):
     today_sr = sun.get_sunrise_time()
     sr_naive = today_sr.replace(tzinfo=None)
     queutime = sr_naive-timenow
-    seconds_queu = int(queutime.total_seconds()) - 36300
+    seconds_queu = int(queutime.total_seconds())
     if seconds_queu <= 10:
         tomorrow_date = date.today() + timedelta(days=1)
         today_sr = sun.get_sunrise_time(tomorrow_date)
         sr_naive = today_sr.replace(tzinfo=None)
         queutime = sr_naive-timenow
-        seconds_queu = int(queutime.total_seconds()) - 36300
-    print(queutime)
+        seconds_queu = int(queutime.total_seconds())
+    print("Update Sunrise | Current time UTC is : ", timenow)
+    print("Update Sunrise | Next Sunset job in : ", queutime)
     context.job_queue.run_once(sunsticker, seconds_queu, chat_id=chat[1], name=str(chat[1])+"sunrise")
 
 def update_job_sunset(chat_id, context:CallbackContext):
+    
+    print("Update Sunset | Chat ", chat_id)
+    
     # Get Chat from database
     localDatabase = Database()
     chat = localDatabase.get_chat(chat_id)
     
     # Remove previous sunset job
+    print("Update Sunset | Try remove sunset for chat ", chat_id)
     job_removed = remove_job_if_exists(str(chat_id)+"sunset", context)
     
     # Get current time
@@ -84,7 +94,8 @@ def update_job_sunset(chat_id, context:CallbackContext):
         ss_naive = today_ss.replace(tzinfo=None)
         queutime = ss_naive-timenow
         seconds_queu = int(queutime.total_seconds())
-    print(queutime)
+    print("Update Sunset | Current time UTC is : ", timenow)
+    print("Update Sunset | Next Sunset job in : ", queutime)
     context.job_queue.run_once(moonsticker, seconds_queu, chat_id=chat[1], name=str(chat[1])+"sunset")
 
 async def start_command(update: Update, context: CallbackContext):
@@ -96,7 +107,7 @@ async def enable_command(update: Update, context: CallbackContext):
     localDatabase = Database()
     chat = localDatabase.get_chat(update.message.chat.id)
 
-    print(chat)
+    print("Enabling chat ", chat)
 
     if chat is None:
         localDatabase.createChat(update.message.chat.id)
@@ -104,10 +115,12 @@ async def enable_command(update: Update, context: CallbackContext):
     update_job_sunrise(chat[1],context)
     update_job_sunset(chat[1],context)
 
-    # current_jobs = context.job_queue.get_jobs_by_name(str(chat[1])+"sunrise")
-    # print(current_jobs)
-    # current_jobs = context.job_queue.get_jobs_by_name(str(chat[1])+"sunset")
-    # print(current_jobs)
+    current_jobs = context.job_queue.get_jobs_by_name(str(chat[1])+"sunrise")
+    print("Current Sunrise Jobs for chat ", chat[1])
+    print(current_jobs)
+    current_jobs = context.job_queue.get_jobs_by_name(str(chat[1])+"sunset")
+    print("Current Sunset Jobs for chat ", chat[1])
+    print(current_jobs)
 
     await update.message.reply_text("Je suis bien activé")
 
@@ -122,7 +135,7 @@ async def longitude_command(update: Update, context: CallbackContext):
         await update.message.reply_text("J'ai ta longitude")
     except:
         await update.message.reply_text("envoie moi un float!")
-    
+    print("Update longitude ", longitude)
     update_job_sunrise(update.message.chat.id,context)
     update_job_sunset(update.message.chat.id,context)
     
@@ -137,7 +150,7 @@ async def latitude_command(update: Update, context: CallbackContext):
         await update.message.reply_text("J'ai ta latitude")
     except:
         await update.message.reply_text("envoie moi un float!")
-    
+    print("Update latitude ", latitude)
     update_job_sunrise(update.message.chat.id,context)
     update_job_sunset(update.message.chat.id,context)
 
@@ -157,7 +170,7 @@ def main():
     localDatabase = Database()
     list_chats = localDatabase.get_chats()
     for chat in list_chats:
-        print(chat)
+        print("Initialisation du chat", chat)
         update_job_sunrise(chat[1],context=application)
         update_job_sunset(chat[1],context=application)
 
